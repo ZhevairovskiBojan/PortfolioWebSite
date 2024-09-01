098616cbb4daa4b3eca03589605487cfb7053ea6
Bojan <128554144+ZhevairovskiBojan@users.noreply.github.com>
2024-09-01 23:03:13 +0200

    Projects

---
+++ src/ProjectsData.js
@@ -0,0 +1,34 @@
+// src/data/projectsData.js
+const projectsData = [
+    {
+        title: 'StockDolphin',
+        description: 'Inventory Stock Supplies System.Technologies: HTML, CSS, JavaScript, ReactJS, NodeJS, ExpressJS, MongoDB',
+        image: `${process.env.PUBLIC_URL}/Chart.jpg`,
+        githubLink: 'https://github.com/ZhevairovskiBojan/ITLabsProject'
+    },
+    {
+        title: 'The Office Chat App',
+        description: 'Social network designed for socializing and exchanging information. Technologies: NodeJS, MongoDB, ExpressJS',
+        image: `${process.env.PUBLIC_URL}/desktop default page.png`, 
+        githubLink: 'https://github.com/ZhevairovskiBojan/OfficeChatApp_Project'
+    },
+
+    {
+        title: 'Apple Redesign Project',
+        description: 'Creative apple website redesighn work with HTML and CSS.',
+        image: `${process.env.PUBLIC_URL}/MacBook.jpg`, 
+        githubLink: 'https://github.com/ZhevairovskiBojan/HTML_CSS-Apple-Site'
+    },
+
+    {
+        title: 'Portfolio Project',
+        description: 'My Portfolio Web Site Project, Technologies: HTML, CSS, JavaScript, React ',
+        image: `${process.env.PUBLIC_URL}/1.png`, 
+        githubLink: 'https://github.com/ZhevairovskiBojan/HTML_CSS-Apple-Site'
+    }
+
+    
+    
+];
+
+export default projectsData;
